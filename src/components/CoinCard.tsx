df874e996685ef8d15ccaca28f23a904a815ac13
Joel Fredericks <jk.fredericks@gmail.com>
2022-02-11 16:37:51 +0200

    Small styling change

--- src/components/CoinCard.tsx
+++ src/components/CoinCard.tsx
@@ -13,8 +13,8 @@ export function CoinCard({ symbol, current_price, price_change_24h, price_change
                 <div className="inline-flex items-center justify-center w-14 h-14 text-blue-100 bg-gradient-to-r rounded-full">
                     <img src={image} alt={`${symbol}`} />
                 </div>
-                <div>
-                    <span className={`font-bold text-${price_change_24h > 0 ? 'green' : 'red'}-400`}>${price_change_24h.toFixed(2)} ({price_change_percentage_24h.toFixed(2)}%)</span><br />
+                <div className={`font-bold text-${price_change_24h > 0 ? 'green' : 'red'}-400`}>
+                    <span>${price_change_24h.toFixed(2)} ({price_change_percentage_24h.toFixed(2)}%)</span><br />
                 </div>
             </div>
             <div>
